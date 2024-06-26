d0ca401859cc424582a25b53e2189f32c4586b46
LuizHVicari <luizhenrique.vicari@gmail.com>
2024-06-26 19:08:45 -0300

    drawer

--- hello_world/lib/pages/login_page.dart
+++ hello_world/lib/pages/login_page.dart
@@ -17,42 +17,69 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return
       Scaffold(
-        body: SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder()
+        body: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyMCAAueSJpJ_56vnMhYRdCIX3Hx_Ag1uJpQ&s', fit: BoxFit.cover,),
+            ),
+            Container(
+              color: Colors.black.withOpacity(.3),
+            ),
+            _body(context)
+          ],
+        ),
+      );
+  }
+
+  SingleChildScrollView _body(BuildContext context) {
+    return SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network('https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2116175301.1719273600&semt=ais_user'),
+                const SizedBox(height: 10),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        TextField(
+                          onChanged: (text) {
+                            email = text;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(), 
+                          ),
+                        ),
+                        const SizedBox(height: 10,),
+                        TextField(
+                          onChanged: (text) {
+                            password = text;
+                          },
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder()
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 10,),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder()
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-                  LoginButton(email: email, password: password)
-                ],
-              )
+                ),
+                const SizedBox(height: 10,),
+                LoginButton(email: email, password: password)
+              ],
             )
-          ),
+          )
         ),
       );
   }
--- hello_world/lib/widgets/home_body.dart
+++ hello_world/lib/widgets/home_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/widgets/switch_theme.dart';
-
+import 'dart:developer';
 
 class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
@@ -19,6 +19,35 @@ class HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: ClipOval(
+                // borderRadius: BorderRadius.circular(40),
+                child: Image.network('https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=')),
+              accountName: const Text('user'), 
+              accountEmail: const Text('email@email.com')
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Início'),
+              subtitle: const Text('Tela de início'),
+              onTap: () {
+                log('Home');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              subtitle: const Text('Finalizar Sessáo'),
+              onTap: () {
+                Navigator.of(context).pushNamed('/');
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Home Page'),
         actions: [SwitchForTheme()],
